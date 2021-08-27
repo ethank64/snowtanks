58b200ae8cdfb4c0bdf2badd580f6c1061a4b5aa
ethank64 <64279690+ethank64@users.noreply.github.com>
2021-08-26 20:15:15 -0400

    Copy/pasted javascript file from my text editor.

---
+++ tank.js
@@ -0,0 +1,67 @@
+class Tank {
+  constructor(x, y, shotAngle, side) {
+    this.initialX = x;
+    this.x = x;
+    this.y = y;
+    this.r = 0;
+    this.shotAngle = shotAngle;
+    this.vel = 0;
+    this.side = side;
+    this.bullets = [];
+    this.otherPlayerHurtbox = createVector(0, 0);
+    this.score = 0;
+  }
+
+  update(up, right, left, down, otherPlayerX, otherPlayerHurtbox) {
+    this.r = ((this.y[floor(this.x) - 4]) - (this.y[floor(this.x) + 4])) * -1;
+    this.vel = 1 - map(abs(this.r), 0, 50, 0, 1);
+    this.otherPlayerHurtbox.set(otherPlayerHurtbox);
+
+    if (keyIsDown(down) && this.shotAngle > -90) {
+      this.shotAngle--;
+    } else if (keyIsDown(up) && this.shotAngle < 90) {
+      this.shotAngle++;
+    }
+    
+    if (keyIsDown(left) && this.x > 16 && (this.x > otherPlayerX || this.side === "left")) {
+      this.x -= this.vel;
+    } else if (keyIsDown(right) && this.x < width - 16 && (this.x < otherPlayerX || this.side === "right")) {
+      this.x += this.vel;
+    }
+    
+    for (let i = 0; i < this.bullets.length; i++) {
+      this.bullets[i].applyForce(gravity);
+      this.bullets[i].update();
+      this.bullets[i].show();
+      
+      if (this.bullets[i].collision(otherPlayerHurtbox, this.y) === "miss") {
+        this.bullets.splice(i, i + 1);
+      } else if (this.bullets[i].collision(otherPlayerHurtbox, this.y) === "hit") {
+        this.score++;
+        reset();
+      }
+    }
+  }
+  
+  shoot(shootButton) {
+    if (keyCode == (shootButton) && this.bullets.length < 3) {
+      this.bullets.push(new Bullet(this.x, this.y[floor(this.x)], this.r + this.shotAngle, 5));
+    }
+  }
+
+  show() {
+    push();
+    strokeWeight(2);
+    stroke(255);
+    noFill();
+    translate(this.x, this.y[floor(this.x)]);
+    rotate(this.r);
+    ellipse(0, 0, 25, 25);
+    ellipse(-8, 28, 10, 10);
+    ellipse(8, 28, 10, 10);
+    rect(0, 18, 30, 10);
+    rotate(this.shotAngle);
+    line(0, -12, 0, -22);
+    pop();
+  }
+}
