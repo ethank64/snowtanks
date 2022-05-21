d7fcff2f6c84f396418aedd0659f415881ef949b
ethank64 <64279690+ethank64@users.noreply.github.com>
2022-05-21 01:21:05 -0400

    Added instructional text.

--- sketch.js
+++ sketch.js
@@ -6,6 +6,9 @@ let player1;
 let player2;
 let player1P;
 let player2P;
+let hidden = false;
+let controls1;
+let controls2;
 
 function setup() {
   const cnv = createCanvas(800, 500);
@@ -32,6 +35,15 @@ function setup() {
   player1P.style("margin", `0% 0% 0% ${(displayWidth - width) / 4}px`);
   player2P.style("margin", `0% ${(displayWidth - width) / 4}px 0% 0%`);
   
+  controls1 = createP("Player 1: A - Left, D - Right, W - Angle Counterclockwise, S - Angle Clockwise, X - Power Down, C - Power Up");
+  controls2 = createP("Player 2: J - Left, L - Right, I - Angle Clockwise, K - Angle Counterclockwise, M - Power Down, N - Power Up")
+  
+  controls1.class("controls");
+  controls2.class("controls");
+  
+  document.getElementsByClassName("controls")[0].onclick = () => {hidden = !hidden};
+  document.getElementsByClassName("controls")[1].onclick = () => {hidden = !hidden};
+  
   player1 = new Tank(floor(width / 6), yLocations, 45, "left");
   player2 = new Tank(floor(width - width / 6), yLocations, -45, "right");
 }
@@ -41,6 +53,14 @@ function draw() {
   
   player1P.html(player1.score);
   player2P.html(player2.score);
+  
+  if (hidden) {
+    controls1.style("visibility", "hidden");
+    controls2.style("visibility", "hidden");
+  } else {
+    controls1.style("visibility", "visible");
+    controls2.style("visibility", "visible");
+  }
 
   for (let i = 0; i < flakes.length; i++) {
     flakes[i].applyForce(gravity);
@@ -54,7 +74,7 @@ function draw() {
   player1.update(83, 68, 65, 87, 67, 88, player2.x - 27, createVector(player2.x, player2.y[floor(player2.x)]));
   player1.show();
   
-  player2.update(73, 76, 74, 75, 77, 78, player1.x + 27, createVector(player1.x, player1.y[floor(player1.x)]));
+  player2.update(73, 76, 74, 75, 78, 77, player1.x + 27, createVector(player1.x, player1.y[floor(player1.x)]));
   player2.show();
 }
 
